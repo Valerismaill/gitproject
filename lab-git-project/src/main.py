6488867ceefc388f7122cd155164b513d747d713
Valerismaill <leronysmaill@gmail.com>
2025-01-23 19:39:07 +0300

    Использование функций add_number и multiply_numbers

--- lab-git-project/src/main.py
+++ lab-git-project/src/main.py
@@ -3,3 +3,15 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+from utils import add_numbers, multiply_numbers
+
+def main():
+    a = 5
+    b = 3
+
+    print(f"The sum of {a} and {b} is: {add_numbers(a, b)}")
+    print(f"The product of {a} and {b} is: {multiply_numbers(a, b)}")
+
+if __name__ == "__main__":
+    main()
